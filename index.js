31c449eaeab0db3ed64b8e46d2312ccb8898b95b
ASRSQ <a.s.rwar@gmail.com>
2026-02-12 20:36:33 -0300

    versão nova index

--- index.js
+++ index.js
@@ -9,19 +9,15 @@ logger.info('Iniciando o bot...');
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
-    executablePath: '/usr/bin/chromium',
-    headless: true,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--single-process',
-      '--disable-gpu'
-    ]
-  }
+  executablePath: '/usr/bin/chromium',
+  headless: true,
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage'
+  ]
+}
+
 });
 
 // QR Code
