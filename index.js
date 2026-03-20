92302e7e3835c02ae4bbddd0ab70376a849810b2
ASRSQ <a.s.rwar@gmail.com>
2026-03-20 11:03:09 -0300

    Voltando para o estado inicial do projeto

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
 // index.js
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const logger = require('./logger');
-require('./database'); // Garante conexão com banco
+require('./database'); // Importa para garantir que o banco de dados conecte
 const { handleCommand } = require('./commandHandler');
 
 logger.info('Iniciando o bot...');
 
 const client = new Client({
-  authStrategy: new LocalAuth(),
-  puppeteer: {
-  executablePath: '/usr/bin/chromium',
-  headless: true,
-  args: [
-    '--no-sandbox',
-    '--disable-setuid-sandbox',
-    '--disable-dev-shm-usage'
-  ]
-}
-
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process', //--no-sandbox and --disable-gpu are the most important
+            '--disable-gpu'
+        ],
+    }
 });
 
-// QR Code
 client.on('qr', qr => {
-  logger.info('QR Code recebido, escaneie com seu celular!');
-  const qrcode = require('qrcode-terminal');
-  qrcode.generate(qr, { small: true });
+    logger.info('QR Code recebido, escaneie com seu celular!');
+    const qrcode = require('qrcode-terminal');
+    qrcode.generate(qr, { small: true });
 });
 
-// Bot pronto
 client.on('ready', () => {
-  logger.info('✅ Bot conectado e pronto para receber comandos!');
+    logger.info('✅ Bot conectado e pronto para receber comandos!');
 });
 
-// Desconexão
 client.on('disconnected', (reason) => {
-  logger.warn(`Bot desconectado: ${reason}`);
+    logger.warn(`Bot desconectado: ${reason}`);
 });
 
-// Mensagens
+// Delega todo o processamento de mensagens para o commandHandler
 client.on('message_create', (message) => {
-  handleCommand(client, message);
+    handleCommand(client, message);
 });
 
-// Inicializa
 client.initialize();
