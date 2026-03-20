0095038b889f1e8491f24733b24747c7c9a1b997
ASRSQ <a.s.rwar@gmail.com>
2026-03-20 17:05:33 -0300

    Att

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
         names: ['alex ramos'], // ← coloque exatamente como aparece no grupo
     },
      PIX_KEY: '88996091894',
-    DEFAULT_RACHA_VALUE: '6,00', // Valor padrão
+    DEFAULT_RACHA_VALUE: '8,00', // Valor padrão
     DEFAULT_MAX_LINHA: 22,
     DEFAULT_MAX_GOLEIROS: 2,
 };
